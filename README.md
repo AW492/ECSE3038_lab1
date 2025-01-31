4d2ea066476a61a7a471045746b74ede1994ce3d
Avanique Weir <studentavaniqueweir@gmail.com>
2025-01-30 22:28:49 -0500

    Question 2 was added by Avanique Weir

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-SUMMARY 1
-The code is to collected a list of 3 resistor values and calculate the parallel resistance and print its output 
+SUMMARY 2
+The code is to take the voltage supplied and the resistor values in series. The total resistance is calculated to find the current which allows for the current to be used to solve for the voltage drop across each individual resistor in the list and is then printed.  
 
-REASON 1
+REASON 2
 Code was written for lab 1 for ECSE3038 course. 
 
 JOKE
-I'm not a hard drinker, I actually find it pretty easy
+I told a chemistry joke once, didnt get much of a reaction
--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-def parallel(list_of_resistances):
-    total = (1/(list_of_resistances[0]))+((1/(list_of_resistances[1]))+((1/list_of_resistances[2])))
-    parallel_resistance = 1/total
-    print("{:.3f}".format(parallel_resistance)+" ohms")
-        
-        
-list =[330, 1000, 2200]
+def potential_divider(voltage,series_resistances):
+    total_resistance = 0
+    for resistance in series_resistances:
+        total_resistance+=resistance
 
-parallel(list)
- 
+    current = voltage/total_resistance
+
+    for resistance in series_resistances:
+        voltage_drop = resistance*current
+        print("{:.2f}".format(voltage_drop)+"v")
+
+list_resistor = [3000,1000]
+
+potential_divider(9,list_resistor)
